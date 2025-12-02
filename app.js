468976abfab86299a0010ec700373af4a0c7dd0f
pilli05 <pillirajesh@ymail.com>
2025-12-02 14:36:18 +0530

    port changes

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://evallo-seven.vercel.app"],
+    origin: ["http://localhost:5173", "https://evallo-seven.vercel.app"],
     credentials: true,
     methods: "GET,POST,PUT,DELETE,PATCH",
   })
