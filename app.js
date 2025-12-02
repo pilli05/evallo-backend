0f8b75e8b47bcd5964a7f19b1bffbd31dd9f818b
pilli05 <pillirajesh@ymail.com>
2025-12-02 14:35:38 +0530

    changes

--- app.js
+++ app.js
@@ -7,12 +7,16 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
-app.use(express.json());
+
 app.use(
   cors({
-    origin: "*",
+    origin: ["http://localhost:3000", "https://evallo-seven.vercel.app"],
+    credentials: true,
+    methods: "GET,POST,PUT,DELETE,PATCH",
   })
 );
+
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 connectDB();
 
